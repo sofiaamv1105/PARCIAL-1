a02093969c68366794838ed000b94fd23913dfe5
sofiaamv1105 <sofiaamv2003@gmail.com>
2025-03-08 23:19:01 -0500

    Update formulario2.php

--- formulario2.php
+++ formulario2.php
@@ -16,6 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Certificaciones Adicionales</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Certificaciones Adicionales</h1>
